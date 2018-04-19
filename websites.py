ae73372e8cd164cb168d75fd7dd00c9c374c9da5
rakesh kumar <rakesh@binarynote.com>
2018-04-19 20:40:55 +0530

    multiple websites using single program

---
+++ websites.py
@@ -0,0 +1,5 @@
+import webbrowser
+webbrowser.open_new_tab("http://www.binarynote.com")
+webbrowser.open_new_tab("clickbank.com")
+webbrowser.open_new_tab("shareasale.com")
+webbrowser.open_new_tab("themeforest.net")
