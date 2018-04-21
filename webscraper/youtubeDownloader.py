436e7b0b62a9e4814c0b48b5bb5e1f44f2620f95
rakesh <rakesh@binarynote.com>
2018-04-21 20:41:16 +0530

    First YouTube Downloader from command line

---
+++ webscraper/youtubeDownloader.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+import youtube_dl
+import urllib
+import shutil
+ydl_opts = {}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=n06H7OcPd-g'])
