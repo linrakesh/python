c68ee0ca7cb56b02362815b81650b507e7f83980
rakesh <rakesh@binarynote.com>
2018-04-21 20:20:19 +0530

    final GUI based imageDownloader from pexels.com

--- webscraper/imagedownloader.py
+++ webscraper/imagedownloader.py
@@ -11,7 +11,13 @@ def downloader(image_url,path):
     file_name = random.randrange(1,10000)
     full_file_name = os.path.join(path,str(file_name) + '.jpg')
     #print(full_file_name)
-    urllib.request.urlretrieve(image_url,full_file_name)
+    r = requests.get(image_url, stream = True)
+    pdf =  open(full_file_name,"wb") 
+    for chunk in r.iter_content(chunk_size=1024):
+         # writing one chunk at a time to pdf file
+         if chunk:
+             pdf.write(chunk)
+    pdf.close()
         
 if( len (sys.argv) > 1):
     query = "+".join(sys.argv[1:])
@@ -33,8 +39,10 @@ for link in soup.select('.js-photo-link img'):
     name = link.get('srcset')
     len = (name.find('?'))
     newname = name[0:len]
-    #print(newname)
+    print(newname)
+    #webbrowser.open(newname)
     downloader(newname,directory)
+print("Download complete check your folder")
 #root = tk.Tk()
 #root.withdraw()
 #directory = filedialog.askdirectory()  #source folder
