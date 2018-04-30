a65f8a438d058e2281871b24b9ee0cb2e669d312
rakesh <rakesh@binarynote.com>
2018-04-30 09:08:06 +0530

    class with variables

--- classes/classDemo.py
+++ classes/classDemo.py
@@ -1,14 +1,14 @@
-class student():
+class student:
     def read_data(self,name,fatherName,stream):
-        name = self.name;
-        fatherName = self.fatherName
-        stream  = self.stream
+        self.name = name
+        self.fatherName = fatherName
+        self.stream = stream
 
     def show_data(self):
         print("Student Name ",self.name)
         print("father  Name ",self.fatherName)
         print("Stream Name ",self.stream)
 
-student s;
+s = student()
 s.read_data('rakesh','jagdish','computer')
 s.show_data()
