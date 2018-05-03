bad46cbdeeb931966a8af15b9df9134e0bd97667
rakesh kumar <rakesh@binarynote.com>
2018-05-03 19:30:51 +0530

    youtube

--- webscraper/youtubeDownloader.py
+++ webscraper/youtubeDownloader.py
@@ -4,4 +4,4 @@ import urllib
 import shutil
 ydl_opts = {}
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://www.youtube.com/watch?v=v8g9arm8nFI&list=PL9rywNkH9WIPqi-YhUlq_pCYaqE0HyaQs'])
+    ydl.download(['https://www.youtube.com/playlist?list=PLgPJX9sVy92yWUMgLpWrXtegKxrWLRnRv'])
