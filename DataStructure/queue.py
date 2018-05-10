896d03f76ce497dea96f667831b92071f917e5f2
rakesh kumar <rakesh@binarynote.com>
2018-05-10 22:53:16 +0530

    data structure queue

--- DataStructure/queue.py
+++ DataStructure/queue.py
@@ -1,32 +1,32 @@
 #function to add element at the end of list
-def push(stack,x):
+def add_element(stack,x):
     stack.append(x)
 #function to remove last element from list    
-def pop(stack):
+def delete_element(stack):
     n = len(stack)
     if(n<=0):
-        print("Stack empty....Pop not possible")
+        print("Queue empty....Deletion not possible")
     else:
-        stack.pop()
+        del(stack[0])
 
 #function to display stack entry
 def display(stack):
     if len(stack)<=0:
-        print("Stack empty...........Nothing to display")
+        print("Queue empty...........Nothing to display")
     for i in stack:
         print(i,end=" ")
 #main program starts from here 
 x=[]
 choice=0
 while (choice!=4):
-    print("\n\t\t\t stack menu \n\t1. push \n\t2. pop \n\t3. Display \n\t4. Exit")
-    choice = int(input("Enter your choice"))
+    print("\n\tQueue menu \n\n\t1. Add Element \n\t2. Delete Element \n\t3. Display \n\t4. Exit")
+    choice = int(input("\n Enter your choice : "))
     
     if(choice==1):
-        value = int(input("Enter value "))
-        push(x,value)
+        value = int(input("Enter value : "))
+        add_element(x,value)
     if(choice==2):
-        pop(x)
+        delete_element(x)
     if(choice==3):
         display(x)
     if(choice==4):
