52c61934292de132ed7c6ff140d475fd4081f2ce
rakesh <rakesh@binarynote.com>
2018-06-17 10:16:13 +0530

    delete record

--- database/delete_record.py
+++ database/delete_record.py
@@ -10,15 +10,10 @@
 #-------------------------------------------------------------------------------
 
 import MySQLdb
-
 db = MySQLdb.connect("localhost","root","ramji","cable")
-
 cursor = db.cursor()
-
 name = input("Enter any name")
-
 sql ="delete from customer where name like '%" + name + "';"
-
 cursor.execute(sql)
 db.commit()
 db.close()
