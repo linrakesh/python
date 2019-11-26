6f871859ab9f22fc4578ad382283c60fcbad4a8d
rakesh <rakesh.linux@gmail.com>
2019-11-26 13:55:40 +0530

    Database prorgam changes

--- Automation/move.py
+++ Automation/move.py
@@ -1,8 +1,17 @@
 import glob
 import os
 import shutil
+import time
+
+source = 'e:/python'
 dest_dir = "C:/test"
-for file in glob.glob('e:/python/*.mp4'):
-    print(file)
-    shutil.copy(file, dest_dir)
-    os.unlink(file)
+
+files = glob.glob(source+"/*.txt")
+
+# for file in files:
+#     print(file)
+#     shutil.copy(file, dest_dir)
+#     os.unlink(file)
+while True:
+    print("Hello rakesh")
+    time.sleep(5)
--- database/tempCodeRunnerFile.py
+++ database/tempCodeRunnerFile.py
@@ -1,2 +1,30 @@
+# -------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      acer
+#
+# Created:     12-04-2018
+# Copyright:   (c) acer 2018
+# Licence:     <your licence>
+# -------------------------------------------------------------------------------
 
-t = PrettyTable(['ID', 'Question', 'A', 'B', 'C',
+import MySQLdb
+from prettytable import PrettyTable
+# Open database connection
+db = MySQLdb.connect("localhost", "root", "", "binarynote")
+cursor = db.cursor()
+sql = "select * from question"
+cursor.execute(sql)
+results = cursor.fetchall()
+t = PrettyTable(['ID', 'Question', 'A', 'B', 'C', 'D',
+                 'Ans', 'Exp', 'gid', 'sid', 'tid'])
+for idr, question, a, b, c, d, ans, exp, gid, sid, tid in results:
+    #idr = record[0]
+    #name = record[1]
+    #fname = record[2]
+    #add = record[3]
+    t.add_row([idr, question, a, b, c, d, ans, exp, gid, sid, tid])
+    # print(idr,name,fname,add,phone,email)
+print(t)
+db.close()
