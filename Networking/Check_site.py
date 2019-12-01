8bcc21308541f5d41e4d243d476fd7ae30fab7e3
rakesh <rakesh.linux@gmail.com>
2019-12-01 23:35:15 +0530

    Networking programs

---
+++ Networking/Check_site.py
@@ -0,0 +1,10 @@
+import os
+
+
+for i in range(2, 255):
+    response = os.system("ping -4t " + hostname)
+    # and then check the response...
+    if response == 0:
+        print(hostname, 'is up!')
+    else:
+        print(hostname, 'is down!')
---
+++ Networking/ftpList.py
@@ -0,0 +1,1 @@
+import ftplib as FTP
