13b4ddef5235c8c177de7c94388e37a5861aa10a
rakesh <rakesh.linux@gmail.com>
2019-12-05 22:41:05 +0530

    new programs

---
+++ numpy/sumOfDD.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+a = np.array([(1, 2, 3), (4, 5, 6), (7, 8, 9)])
+b = np.sum(a, 1)
+print(b)
+
+``
---
+++ webscraper/wiki.py
@@ -0,0 +1,4 @@
+import wikipedia as wi
+print(wi.summary('wikipedia'))
+result = wi.page('rakesh kumar')
+print(result.url)
